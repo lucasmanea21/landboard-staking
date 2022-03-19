d48e57c15b657d4e5bb97a26b21006bf269e683f
alex-streza <alex.streza@okapistudio.com>
2022-03-19 19:42:23 +0200

    changes

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,37 +1,61 @@
+import { sendTransactions } from "@elrondnetwork/dapp-core";
+import { Address, ContractFunction, GasLimit, SmartContract } from "@elrondnetwork/erdjs/out";
 import axios from "axios";
+import { CALLER_ADDRESS } from "config";
 
 interface GetLatestTransactionsType {
-	apiAddress: string;
-	address: string;
-	contractAddress: string;
-	timeout: number;
-	page?: number;
-	url?: string;
+  apiAddress: string;
+  address: string;
+  contractAddress: string;
+  timeout: number;
+  page?: number;
+  url?: string;
 }
 
 const fetchTransactions = (url: string) =>
-	async function getTransactions({ apiAddress, address, contractAddress, timeout }: GetLatestTransactionsType) {
-		try {
-			const { data } = await axios.get(`${apiAddress}${url}`, {
-				params: {
-					sender: address,
-					receiver: contractAddress,
-					condition: "must",
-					size: 25,
-				},
-				timeout,
-			});
+  async function getTransactions({
+    apiAddress,
+    address,
+    contractAddress,
+    timeout,
+  }: GetLatestTransactionsType) {
+    try {
+      const { data } = await axios.get(`${apiAddress}${url}`, {
+        params: {
+          sender: address,
+          receiver: contractAddress,
+          condition: "must",
+          size: 25,
+        },
+        timeout,
+      });
 
-			return {
-				data: data,
-				success: data !== undefined,
-			};
-		} catch (err) {
-			return {
-				success: false,
-			};
-		}
-	};
+      return {
+        data: data,
+        success: data !== undefined,
+      };
+    } catch (err) {
+      return {
+        success: false,
+      };
+    }
+  };
 
 export const getTransactions = fetchTransactions("/transactions");
 export const getTransactionsCount = fetchTransactions("/transactions/count");
+
+const createStakeTransaction = async (senderAddress: string) => {
+  let contract = new SmartContract({
+    address: new Address(CALLER_ADDRESS),
+  });
+
+  let tx = contract.call({
+    func: new ContractFunction("transferToken"),
+    gasLimit: new GasLimit(5000000),
+    // args: [new AddressValue(TOKE_ID), STAKE_ONLY_HEX, senderAddress],
+  });
+
+  sendTransactions({
+    transactions: tx,
+  });
+};
--- src/config.tsx
+++ src/config.tsx
@@ -1,2 +1,19 @@
-export const contractAddress = "erd1qqqqqqqqqqqqqpgqljx7ahxgz60k5tn5h7rs94z9ukwudvgrstrqz3lhny";
 export const dAppName = "LandBoard Staking";
+
+export const CALLER_ADDRESS =
+  "erd1dl8ucerztz80eqtvs2u35vj5pckle3h3mnuce5fctyzxp4d74dfqwy7ntn";
+export const CALLER_ADDRESS_HEX =
+  "0x$(erdpy wallet bech32 --decode ${CALLER_ADDRESS})";
+export const CALLER_ADDRESS_ONLY_HEX =
+  "$(erdpy wallet bech32 --decode ${CALLER_ADDRESS})";
+
+export const TOKEN_ID = "SVEN-4b35b0";
+export const TOKEN_ID_HEX = "0x$(echo -n ${TOKEN_ID} | xxd -p -u | tr -d '\n')";
+export const TOKEN_ID_ONLY_HEX =
+  "$(echo -n ${TOKEN_ID} | xxd -p -u | tr -d '\n')";
+export const REFERRRAL_ACTIVATION_AMOUNT = 100000000000000000000;
+export const APY_INCREASE_PER_REFERRAL = 50;
+export const MAX_APY_INCREASE_BY_REFERRAL = 1000;
+
+export const STAKE = "stake";
+export const STAKE_ONLY_HEX = "$(echo -n ${STAKE} | xxd -p -u | tr -d '\n')";
